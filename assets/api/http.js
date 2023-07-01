80ebe631dcdb81edf3a4cca620cf08fbfb8e46b8
livinoid98 <ccy9803@naver.com>
2023-07-02 03:22:47 +0900

    change localhost

--- assets/api/http.js
+++ assets/api/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://127.0.0.1:8080",
   headers: {
     "Content-Type": "application/json;charset=utf-8",
   },
