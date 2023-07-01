0bde4de3f26c5236e9d8ca68af324563cc39638c
livinoid98 <ccy9803@naver.com>
2023-07-02 01:57:50 +0900

    add sh

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       repo : 'git@github.com:livinoid98/ssabang-nuxt.git',
       path : '/home/ubuntu',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && npm run build && npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
