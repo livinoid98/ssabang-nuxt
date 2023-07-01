bd0ad5d4d17599689bce6239a624076bd58e87e6
livinoid98 <ccy9803@naver.com>
2023-07-02 00:50:56 +0900

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    script: 'npm start'
+  }],
+
+  deploy : {
+    production : {
+      key : 'SSABANG.pem',
+      user : 'ubuntu',
+      host : '3.34.3.158',
+      ref  : 'origin/master',
+      repo : 'git@github.com:livinoid98/ssabang-nuxt.git',
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
